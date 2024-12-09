0de9d88f997339c3959a63447ab6c7d0f1ba9206
User <User@DESKTOP-MNJEUTV>
2024-12-09 14:57:39 +0500

    а

---
+++ Logger.cs
@@ -0,0 +1,7 @@
+﻿public class Log
+{
+    private string messege;
+
+
+
+}
--- Programm.cs
+++ Programm.cs
@@ -5,13 +5,7 @@ public class Program
     {
         manegerBD meneger = new manegerBD("C:\\Users\\User\\Desktop\\LR5 Excel Files\\LR5-var9.xls");
 
-        //meneger.OutputBD();
-
-        //meneger.DeleteElementFromReservation(97560);
-        //meneger.CorrectionElement(123);
-        //meneger.OutputBD();
-        meneger.GetRevenueForCity("г. Тында");
-        meneger.GetReservationOnClients();
+        meneger.OutputBD();
     }
 
 }
